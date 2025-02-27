8d44cfab38211ac727c8fa31f0d4dcf22694382d
ben59 <benjamin.lize@eleve.ensai.fr>
2025-02-27 11:49:35 +0000

    tp2

--- TP2_Stats_Spatiales.R
+++ TP2_Stats_Spatiales.R
@@ -33,6 +33,44 @@ dept_bretagne<-communes_Bretagne %>%
   summarise(surf=sum(surf))
 plot(dept_bretagne)
 
-L’objectif est de créer une table départementale “dept_bretagne” sans doublons. Cette table devra
-contenir le code departement et la superficie du département. Représenter le nouveau fond sur une
-carte avec la fonction plot
+#Question 13 :
+dept_bretagne2<-communes_Bretagne %>% 
+  group_by(dep) %>% 
+  summarise(st_union(geom))
+plot(dept_bretagne2)
+
+#Question 14 :
+centroid_dept_bret<-st_centroid(dept_bretagne)
+str(centroid_dept_bret)
+plot(st_geometry(dept_bretagne))
+plot(st_geometry(centroid_dept_bret),add=TRUE)
+dept_lib=data.frame(
+  dep=c("22","29","35","56"),
+  dep_lib=c("Côte d'Armor","Finistère","Ille-et-Vilaine","Morbihan")
+)
+centroid_dept_bret<-centroid_dept_bret %>% 
+  left_join(dept_lib,
+            by="dep")
+
+centroid_coord<-st_coordinates(centroid_dept_bret)
+centroid_coord %>% str()
+
+centroid_coord<-centroid_coord %>% 
+  bind_cols(... = centroid_dept_bret %>% 
+              select(dep,dep_lib) %>% 
+              st_drop_geometry()
+            )
+centroid_coord %>% str()
+
+plot(st_geometry(dept_bretagne))
+plot(st_geometry(centroid_dept_bret),pch=16,col='orangered',add=TRUE)
+text(
+  x=centroid_coord$X,
+  y=centroid_coord$Y,
+  labels=centroid_coord$dep_lib,
+  pos=3,
+  cex=0.8,
+  col="coral"
+)
+commune_centroid_bret<-st_intersection(communes_Bretagne,centroid_dept_bret)
+typeof(commune_centroid_bret)
